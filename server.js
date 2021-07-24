807a968f2c96f3b752e339bc6e457a318d40df17
Muhamad Firhat <muhamad.firhat@gmail.com>
2021-07-24 10:43:06 +0700

    add travis env

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
+
 function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
 
+const favoriteFood = process.env.FAVORITE_FOOD;
+
 async function main() {
   while(true) {
-    console.log('Containers rule!');
+    console.log("Microservices rock!");
+	console.log(`My Favorite food is ${favoriteFood}`);
     await sleep(5000);
   }
 }
